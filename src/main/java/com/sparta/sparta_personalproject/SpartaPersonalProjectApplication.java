c9e3248a9020d35c79251f65aed11d9c604a7940
jiho <shl05234@naver.com>
2022-12-07 19:19:18 +0900

    first commit

---
+++ src/main/java/com/sparta/sparta_personalproject/SpartaPersonalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaPersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaPersonalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/sparta_personalproject/SpartaPersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_personalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaPersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
